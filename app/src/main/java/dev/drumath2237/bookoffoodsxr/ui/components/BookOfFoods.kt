10ccec2b3497d26e8f8a8beb46849221e9603a3e
drumath2237 <2237k.drumath@gmail.com>
2025-05-22 04:10:04 +0900

    :tada: シーンチェンジして3Dコンテンツを出せた

--- app/src/main/java/dev/drumath2237/bookoffoodsxr/ui/components/BookOfFoods.kt
+++ app/src/main/java/dev/drumath2237/bookoffoodsxr/ui/components/BookOfFoods.kt
@@ -1,6 +1,5 @@
 package dev.drumath2237.bookoffoodsxr.ui.components
 
-import android.app.UiModeManager
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -22,6 +21,8 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
@@ -32,24 +33,44 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.UiMode
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import androidx.xr.compose.platform.LocalSession
+import androidx.xr.compose.platform.LocalSpatialCapabilities
+import androidx.xr.compose.spatial.Orbiter
+import androidx.xr.compose.spatial.OrbiterEdge
+import androidx.xr.compose.spatial.Subspace
+import androidx.xr.compose.subspace.SpatialPanel
+import androidx.xr.compose.subspace.layout.SubspaceModifier
+import androidx.xr.compose.subspace.layout.offset
+import androidx.xr.compose.subspace.layout.size
+import androidx.xr.runtime.math.Pose
+import androidx.xr.runtime.math.Vector3
+import androidx.xr.scenecore.GltfModel
+import androidx.xr.scenecore.GltfModelEntity
+import androidx.xr.scenecore.MovableComponent
+import androidx.xr.scenecore.scene
 import dev.drumath2237.bookoffoodsxr.R
 import dev.drumath2237.bookoffoodsxr.data.FoodResource
 import dev.drumath2237.bookoffoodsxr.data.foodsResources
 import dev.drumath2237.bookoffoodsxr.ui.theme.AndroidXRBookOfFoodsTheme
+import kotlinx.coroutines.guava.await
 
 @Composable
 fun BookOfFoods() {
     var foodIndex by remember { mutableIntStateOf(0) }
     val food = foodsResources[foodIndex]
 
-    FoodScreen(
-        food = food,
-        onNextButtonClicked = { foodIndex = (foodIndex + 1) % foodsResources.size }
-    )
+    if (LocalSpatialCapabilities.current.isSpatialUiEnabled) {
+        SpatialFoodScreen(food = food)
+    } else {
+        FoodScreen(
+            food = food,
+            onNextButtonClicked = { foodIndex = (foodIndex + 1) % foodsResources.size },
+        )
+
+    }
 
 }
 
@@ -58,12 +79,12 @@ fun FoodScreen(
     food: FoodResource,
     modifier: Modifier = Modifier,
     onNextButtonClicked: () -> Unit,
-    onOpenInFullButtonClicked: () -> Unit = {},
 ) {
+    val session = LocalSession.current
+
     Surface {
         Box(
-            contentAlignment = Alignment.Center,
-            modifier = modifier.fillMaxSize()
+            contentAlignment = Alignment.Center, modifier = modifier.fillMaxSize()
         ) {
             Row(
                 horizontalArrangement = Arrangement.spacedBy(16.dp),
@@ -80,7 +101,7 @@ fun FoodScreen(
                     )
 
                     IconButton(
-                        onClick = onOpenInFullButtonClicked,
+                        onClick = { session?.scene?.spatialEnvironment?.requestFullSpaceMode() },
                         modifier = Modifier
                             .align(Alignment.TopEnd)
                             .padding(8.dp)
@@ -100,7 +121,22 @@ fun FoodScreen(
                     }
                 }
 
-                FoodDetailSidePanel(food = food, onNextButtonClicked = onNextButtonClicked)
+                FoodDetailSidePanel(food = food) {
+                    Button(
+                        onClick = onNextButtonClicked,
+                        colors = ButtonColors(
+                            containerColor = Color(0xFFB09257),
+                            contentColor = Color.White,
+                            disabledContainerColor = Color.Gray,
+                            disabledContentColor = contentColorFor(Color.Gray)
+                        ), modifier = Modifier
+                            .padding(vertical = 16.dp)
+                            .width(120.dp)
+                            .height(56.dp)
+                    ) {
+                        Text("Next", fontSize = TextUnit(22f, TextUnitType.Sp))
+                    }
+                }
             }
         }
     }
@@ -110,7 +146,7 @@ fun FoodScreen(
 fun FoodDetailSidePanel(
     food: FoodResource,
     modifier: Modifier = Modifier,
-    onNextButtonClicked: () -> Unit,
+    additionalComponent: @Composable () -> Unit = {}
 ) {
     Column(modifier = modifier) {
         Text(
@@ -125,34 +161,66 @@ fun FoodDetailSidePanel(
             modifier = Modifier.padding(vertical = 16.dp)
         )
 
-        Button(
-            onClick = onNextButtonClicked,
-            colors = ButtonColors(
-                containerColor = Color(0xFFB09257),
-                contentColor = Color.White,
-                disabledContainerColor = Color.Gray,
-                disabledContentColor = contentColorFor(Color.Gray)
-            ),
-            modifier = Modifier
-                .padding(vertical = 16.dp)
-                .width(120.dp)
-                .height(56.dp)
+        additionalComponent()
+    }
+}
 
+@Composable
+fun SpatialFoodScreen(
+    food: FoodResource
+) {
+    Subspace {
+        val xrSession = checkNotNull(LocalSession.current)
+        var gltfEntity = remember<GltfModelEntity?> { null }
+
+        SpatialPanel(
+            modifier = SubspaceModifier
+                .size(256.dp)
+                .offset(y = (-120).dp)
         ) {
-            Text("Next", fontSize = TextUnit(22f, TextUnitType.Sp))
+            LaunchedEffect(key1 = Unit) {
+                val gltfModel = GltfModel.create(xrSession, name = food.glbModelPath).await()
+                gltfEntity = GltfModelEntity.create(
+                    session = xrSession,
+                    model = gltfModel,
+                    pose = Pose(translation = Vector3(0f, -0.15f, 0f))
+                )
+                gltfEntity.addComponent(MovableComponent.create(session = xrSession))
+            }
+
+            DisposableEffect(Unit) {
+                onDispose {
+                    gltfEntity?.dispose()
+                }
+            }
+
+            Orbiter(
+                position = OrbiterEdge.Bottom,
+            ) {
+                Button(onClick = { xrSession.scene.spatialEnvironment.requestHomeSpaceMode() }) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_home_space_mode_switch),
+                            contentDescription = "return to 2d"
+                        )
+                        Text("Return to 2D")
+                    }
+                }
+            }
         }
     }
 }
 
 @Preview(
-    widthDp = 1024,
-    heightDp = 720,
+    widthDp = 1024, heightDp = 720,
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true
 )
 @Preview(
-    widthDp = 1024,
-    heightDp = 720,
+    widthDp = 1024, heightDp = 720,
     uiMode = Configuration.UI_MODE_NIGHT_NO,
     showBackground = true
 )
