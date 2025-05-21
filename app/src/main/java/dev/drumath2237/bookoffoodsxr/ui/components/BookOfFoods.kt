eeca0f810fba8a0b89cf6ee408282478dee26754
drumath2237 <2237k.drumath@gmail.com>
2025-05-22 02:29:58 +0900

    :sparkles: 2D UIのデザインをしっかりやった

--- app/src/main/java/dev/drumath2237/bookoffoodsxr/ui/components/BookOfFoods.kt
+++ app/src/main/java/dev/drumath2237/bookoffoodsxr/ui/components/BookOfFoods.kt
@@ -5,11 +5,16 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,7 +28,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import dev.drumath2237.bookoffoodsxr.R
 import dev.drumath2237.bookoffoodsxr.data.FoodResource
 import dev.drumath2237.bookoffoodsxr.data.foodsResources
 import dev.drumath2237.bookoffoodsxr.ui.theme.AndroidXRBookOfFoodsTheme
@@ -43,31 +51,72 @@ fun BookOfFoods() {
 @Composable
 fun FoodScreen(
     food: FoodResource,
+    modifier: Modifier = Modifier,
     onNextButtonClicked: () -> Unit,
-    modifier: Modifier = Modifier
+    onOpenInFullButtonClicked: () -> Unit = {},
 ) {
     Surface {
         Box(
             contentAlignment = Alignment.Center,
             modifier = modifier.fillMaxSize()
         ) {
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                modifier = Modifier.padding(16.dp)
             ) {
-                Text(text = food.name, modifier = Modifier.padding(16.dp))
+                Box(modifier = Modifier.padding(8.dp)) {
+                    Image(
+                        painter = painterResource(food.imageResourceId),
+                        contentDescription = food.name,
+                        modifier = Modifier
+                            .size(480.dp)
+                            .clip(RoundedCornerShape(32.dp))
+                            .background(color = Color(217, 194, 163, 255))
+                    )
 
-                Image(
-                    painter = painterResource(food.imageResourceId),
-                    contentDescription = food.name,
-                    modifier = Modifier
-                        .size(250.dp) // ← ここが幅（高さも同じになる）
-                        .clip(RoundedCornerShape(16.dp))
-                        .background(color = Color(217, 194, 163, 255))
-                )
+                    IconButton(
+                        onClick = onOpenInFullButtonClicked,
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(8.dp)
 
-                Button(onClick = onNextButtonClicked, modifier = Modifier.padding(16.dp)) {
-                    Text("Next")
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.outline_open_in_full_24),
+                            contentDescription = "open in full",
+                            tint = Color.White,
+                            modifier = Modifier
+                                .size(64.dp)
+                                .clip(RoundedCornerShape(32.dp))
+                                .background(color = Color(0xFFB09257))
+                                .padding(8.dp)
+
+                        )
+                    }
+                }
+
+                Column {
+                    Text(
+                        text = food.name,
+                        fontSize = TextUnit(45f, TextUnitType.Sp),
+                        modifier = Modifier.padding(vertical = 16.dp)
+                    )
+
+                    Text(
+                        food.detail,
+                        fontSize = TextUnit(22f, TextUnitType.Sp),
+                        modifier = Modifier.padding(vertical = 16.dp)
+                    )
+
+                    Button(
+                        onClick = onNextButtonClicked,
+                        modifier = Modifier
+                            .padding(vertical = 16.dp)
+                            .width(120.dp)
+                            .height(56.dp)
+                    ) {
+                        Text("Next", fontSize = TextUnit(22f, TextUnitType.Sp))
+                    }
                 }
             }
         }
